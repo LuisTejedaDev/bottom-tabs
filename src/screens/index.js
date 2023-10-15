3dbbff8d76ca06645b09f0b68d68fa569bd4f863
LuisTejedaDev <ltejeda063099@gmail.com>
2023-10-15 17:18:44 -0600

    First Commit

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export {default as Chat} from './Chat'
+export {default as Home} from './Home'
+export {default as Likes} from './Likes'
+export {default as Notifications} from './Notifications'
+export {default as Config} from './Config'
